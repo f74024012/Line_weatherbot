69c5585dfdefac9f38e50d198650fd28ef3529eb
F74024012 <james22664505@gmail.com>
2016-12-31 16:29:08 +0800

    tainan9

--- line_echobot/echobot/views.py
+++ line_echobot/echobot/views.py
@@ -69,7 +69,7 @@ def callback(request):
     city=["臺北市","新北市","桃園市","臺中市","臺南市",
           "高雄市","基隆市","新竹市","嘉義市","新竹縣","苗栗縣",
           "彰化縣","南投縣","雲林縣","嘉義縣","屏東縣","宜蘭縣",
-          "花蓮縣","臺東縣","臺東市","澎湖縣","金門縣","連江縣"]
+          "花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"]
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
