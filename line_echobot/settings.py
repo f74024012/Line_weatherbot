005689bf68dde2b990d0bc46bf2ea2a5d114e7ec
F74024012 <james22664505@gmail.com>
2017-01-03 23:38:52 +0800

    change settings

--- line_echobot/settings.py
+++ line_echobot/settings.py
@@ -15,7 +15,6 @@ import os
 from util import get_env_variable
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
